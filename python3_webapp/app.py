cba0a15210c950a60eb49dc38e82b977efe0e4e0
ziguochen.git <ziguochen@163.com>
2017-08-04 17:44:45 +0800

    add web python

---
+++ python3_webapp/app.py
@@ -0,0 +1,33 @@
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio,os,json,time
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+	return web.Response(body=b'<h1>Awesome</h1>')
+
+@asyncio.coroutine
+def create_pool(loop,**kw):
+	logging.info('create database connection pool...')
+	global __pool
+	__pool = yield from aiomysql.create_pool(
+		host=kw.get('host','localhost'),
+		port=kw.get('port',3306),
+		user=kw['user'],
+		password=kw['password'],
+		db=kw['db'],
+		charset=kw.get('
+
+
+@asyncio.coroutine
+def init(loop):
+	app = web.Application(loop=loop)
+	app.router.add_route('GET','/',index)
+	srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+	logging.info('server started at http://127.0.0.1:9000...')
+	return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
